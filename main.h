fb2a078eabc20bfbdcbb8d960d77ccfbc742ecb8
dunsinadesina <adesinajesudunsin@gmail.com>
2023-09-16 11:12:34 -0700

    'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+int _printf(const char *format, ...);
+int _strlen(const char *str);
+#endif/*MAIN_H*/
